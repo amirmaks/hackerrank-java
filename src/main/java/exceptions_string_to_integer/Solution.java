db621ef0925fcd7ec58ed93960e29d0889442f26
Makhsat Amirov <amriovmaks@gmail.com>
2019-02-22 16:28:50 +0600

    30 days of code

---
+++ src/main/java/exceptions_string_to_integer/Solution.java
@@ -0,0 +1,16 @@
+package exceptions_string_to_integer;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try {
+            int result = Integer.parseInt(S);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+    }
+}
