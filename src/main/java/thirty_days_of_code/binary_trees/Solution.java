30ca0e98062b9e7b0f469a2ef9c8d5d08cd4a195
Makhsat Amirov <amriovmaks@gmail.com>
2019-03-05 17:18:58 +0600

    30 days of code

---
+++ src/main/java/thirty_days_of_code/binary_trees/Solution.java
@@ -0,0 +1,78 @@
+package thirty_days_of_code.binary_trees;
+import com.sun.deploy.util.SyncAccess;
+
+import java.util.*;
+import java.io.*;
+
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+
+    static void levelOrder(Node root){
+        if (root == null) {
+            return;
+        }
+
+        ArrayList<Node> arrayList = new ArrayList<Node>();
+
+        arrayList.add(root);
+
+        while (arrayList.size() > 0) {
+            Node tree = arrayList.remove(0);
+            System.out.print(tree.data + " ");
+
+            if (tree.left != null) {
+                arrayList.add(tree.left);
+            }
+
+            if (tree.right != null) {
+                arrayList.add(tree.right);
+            }
+        }
+
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+//        Scanner sc=new Scanner(System.in);
+//        int T=sc.nextInt();
+//        Node root=null;
+//        while(T-->0){
+//            int data=sc.nextInt();
+//            root=insert(root,data);
+//        }
+//        levelOrder(root);
+
+        Node root=null;
+        root = insert(root, 3);
+        root = insert(root, 5);
+        root = insert(root, 4);
+        root = insert(root, 7);
+        root = insert(root, 2);
+        root = insert(root, 1);
+        levelOrder(root);
+    }
+}
