00797f61161ad188aeb7dcf542859f6c01f3e7b3
Makhsat Amirov <amriovmaks@gmail.com>
2019-04-01 14:32:12 +0600

    selection sort

--- src/main/java/hackerearth/alghoritms/sorting/selection_sort/TestClass.java
+++ src/main/java/hackerearth/alghoritms/sorting/selection_sort/TestClass.java
@@ -1,26 +1,33 @@
-package hackerearth.alghoritms.sorting.bubble_sort;
+package hackerearth.alghoritms.sorting.selection_sort;
 
 import java.util.Arrays;
 
 public class TestClass {
     public static void main(String[] args) {
-        int[] A = {7, 4, 5, 2};
+        int[] A = {7,5,4,2};
         process(A, A.length);
     }
 
     public static void process(int[] A, int n) {
-        int temp;
+
+        int minimum;
         int iterations = 0;
 
-        for (int k = 0; k < n - 1; k++) {
-            for (int i = 0; i < n - k - 1; i++) {
-                if (A[i] > A[i+1]) {
-                    temp = A[i];
-                    A[i] = A[i+1];
-                    A[i+1] = temp;
+
+        for (int i = 0; i < n-1; i++) {
+            minimum = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if(A[j] < A[minimum]) {
+                    minimum = j;
                     iterations++;
                 }
             }
+
+            int temp = A[i];
+
+            A[i] = A[minimum];
+            A[minimum] = temp;
         }
 
         System.out.println(Arrays.toString(A));
